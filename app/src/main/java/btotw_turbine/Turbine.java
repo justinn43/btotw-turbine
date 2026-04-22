900828b0a219bf74c8a60820ca5cf277c6a4ed1c
justinn43 <justinn43@vt.edu>
2026-04-22 16:59:28 -0400

    added turbine representation

--- app/src/main/java/btotw_turbine/Turbine.java
+++ app/src/main/java/btotw_turbine/Turbine.java
@@ -2,7 +2,29 @@ package btotw_turbine;
 
 public class Turbine {
     
+    private float windSpeed; // m/s
+    private float torque;
+    private float power;
+    
     public Turbine() {
 
     }
+
+
+    /**
+     * 
+     * @return the current wind speed in m/s
+     */
+    public float getCurrentWindSpeed() {
+        return this.windSpeed;
+    }
+    /**
+     * 
+     * @param windSpeed
+     */
+    public void updateWindSpeed(float windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+
 }
